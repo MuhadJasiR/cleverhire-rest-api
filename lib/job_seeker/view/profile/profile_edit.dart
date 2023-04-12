bc4815716464f29ea1756c999c8534a106f16bcf
MuhadJasiR <alijasir051@gmail.com>
2023-04-12 22:00:09 +0530

    completed upto comment session

--- lib/job_seeker/view/profile/profile_edit.dart
+++ lib/job_seeker/view/profile/profile_edit.dart
@@ -56,132 +56,135 @@ class ProfileEdit extends StatelessWidget {
               icon: const Icon(Icons.exit_to_app))
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Consumer2<LocalFunctionProvider, SeekerLoginProvider>(
-          builder: (context, provider1, provider, child) => Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CircleAvatar(
-                radius: 60,
-                backgroundColor: kMainColor,
-                child: GestureDetector(
-                  onTap: () {
-                    provider1.pickImage();
-                  },
-                  child: ClipOval(
-                      child: provider1.imageFromGallery == null
-                          ? Image.asset("assets/profile.png")
-                          : Image.file(
-                              provider1.imageFromGallery!,
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.cover,
-                            )),
-                ),
-              ),
-              kHeight(15),
-              const Text("Upload profile photo"),
-              kHeight(10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  kHeight(15),
-                  const Text("Date of birth"),
-                  kHeight(5),
-                  TextFormField(
-                    keyboardType: TextInputType.none,
-                    // controller: value.dateController,
-                    onTap: () async {
-                      provider.datePick(context);
+      body: SizedBox(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Consumer2<LocalFunctionProvider, SeekerLoginProvider>(
+            builder: (context, provider1, provider, child) => Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(
+                  radius: 60,
+                  backgroundColor: kMainColor,
+                  child: GestureDetector(
+                    onTap: () {
+                      provider1.pickImage();
                     },
-                    decoration: InputDecoration(
-                        hintText: provider.selectedDate ?? "select date",
-                        suffixIcon: Icon(
-                          Icons.calendar_month_rounded,
-                          size: 25,
-                          color: kTextFieldColor,
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: kMainColor),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.red),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        isDense: true),
-                  ),
-                  kHeight(15),
-                  const Text("address"),
-                  kHeight(5),
-                  TextFormField(
-                    controller: provider.addressController,
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: kMainColor),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.red),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        isDense: true),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "please enter address";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
-                  kHeight(15),
-                  const Text("Occupation"),
-                  kHeight(5),
-                  TextFormField(
-                    controller: provider.occupationController,
-                    decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: kMainColor),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)),
-                        errorBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.red),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        isDense: true),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Please enter occupation";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
-                  kHeight(20),
-                ],
-              ),
-              const Spacer(),
-              SizedBox(
-                width: 200,
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: kMainColor),
-                  onPressed: () {},
-                  child: const Text(
-                    "Confirm",
-                    style: TextStyle(fontSize: 17, color: kWhiteColor),
+                    child: ClipOval(
+                        child: provider1.imageFromGallery == null
+                            ? Image.asset("assets/profile.png")
+                            : Image.file(
+                                provider1.imageFromGallery!,
+                                width: 150,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              )),
                   ),
                 ),
-              ),
-              kHeight(20)
-            ],
+                kHeight(15),
+                const Text("Upload profile photo"),
+                kHeight(10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    kHeight(15),
+                    const Text("Date of birth"),
+                    kHeight(5),
+                    TextFormField(
+                      keyboardType: TextInputType.none,
+                      // controller: value.dateController,
+                      onTap: () async {
+                        provider.datePick(context);
+                      },
+                      decoration: InputDecoration(
+                          hintText: provider.selectedDate ?? "select date",
+                          suffixIcon: Icon(
+                            Icons.calendar_month_rounded,
+                            size: 25,
+                            color: kTextFieldColor,
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: kMainColor),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          isDense: true),
+                    ),
+                    kHeight(15),
+                    const Text("address"),
+                    kHeight(5),
+                    TextFormField(
+                      controller: provider.addressController,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: kMainColor),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          isDense: true),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "please enter address";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    kHeight(15),
+                    const Text("Occupation"),
+                    kHeight(5),
+                    TextFormField(
+                      controller: provider.occupationController,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: kMainColor),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.red),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          isDense: true),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter occupation";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    kHeight(20),
+                  ],
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: kMainColor),
+                    onPressed: () {},
+                    child: const Text(
+                      "Update",
+                      style: TextStyle(fontSize: 17, color: kWhiteColor),
+                    ),
+                  ),
+                ),
+                kHeight(20)
+              ],
+            ),
           ),
         ),
       ),
