ffe3bfcc8cc06cfaf41a8440d0b90137b2497053
MuhadJasiR <alijasir051@gmail.com>
2023-04-17 15:45:41 +0530

    created upto getting chat model

--- lib/recruiter/view/chat/recruiter_chat_screen.dart
+++ lib/recruiter/view/chat/recruiter_chat_screen.dart
@@ -1,15 +1,24 @@
 import 'package:cleverhire/core/color/color.dart';
 import 'package:cleverhire/core/constraints/constraints.dart';
+import 'package:cleverhire/recruiter/controller/provider/get_all_chats_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'recuiter_message_screen.dart';
+import 'recruiter_message_screen.dart';
 
 class RecruiterChatScreen extends StatelessWidget {
   const RecruiterChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        Provider.of<GetAllChatsProvider>(context, listen: false)
+            .fetchingAllChats();
+      },
+    );
+
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
@@ -23,52 +32,60 @@ class RecruiterChatScreen extends StatelessWidget {
               children: [
                 const CupertinoSearchTextField(),
                 kHeight(10),
-                ListView.separated(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) => GestureDetector(
-                          onTap: () =>
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => RecruiterMessageScreen(
-                                        usersId: "",
-                                      ))),
-                          child: Card(
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                radius: 30,
-                                child: Image.asset(
-                                  "assets/profile.png",
+                Consumer<GetAllChatsProvider>(builder: (context, value, child) {
+                  return value.isLoading
+                      ? const Center(
+                          child: CircularProgressIndicator(),
+                        )
+                      : ListView.separated(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) => GestureDetector(
+                                onTap: () => Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            RecruiterMessageScreen(
+                                              chatId: value.chatId!,
+                                            ))),
+                                child: Card(
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      radius: 30,
+                                      child: Image.asset(
+                                        "assets/profile.png",
+                                      ),
+                                    ),
+                                    title: Text(
+                                        value.filteredChats[index].username),
+                                    subtitle: const Text("FLutter developer"),
+                                    trailing: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Text(
+                                          "2:40 PM",
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                        CircleAvatar(
+                                          backgroundColor:
+                                              kMainColor.withOpacity(0.8),
+                                          radius: 10,
+                                          child: const Center(
+                                              child: Text(
+                                            "2",
+                                            style: TextStyle(fontSize: 10),
+                                          )),
+                                        )
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
-                              title: const Text("Jasir"),
-                              subtitle: const Text("FLutter developer"),
-                              trailing: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  const Text(
-                                    "2:40 PM",
-                                    style: TextStyle(fontSize: 12),
-                                  ),
-                                  CircleAvatar(
-                                    backgroundColor:
-                                        kMainColor.withOpacity(0.8),
-                                    radius: 10,
-                                    child: const Center(
-                                        child: Text(
-                                      "2",
-                                      style: TextStyle(fontSize: 10),
-                                    )),
-                                  )
-                                ],
+                          separatorBuilder: (context, index) => const Divider(
+                                thickness: 0.3,
                               ),
-                            ),
-                          ),
-                        ),
-                    separatorBuilder: (context, index) => const Divider(
-                          thickness: 0.3,
-                        ),
-                    itemCount: 5)
+                          itemCount: value.filteredChats.length);
+                })
               ],
             )
           ]),
